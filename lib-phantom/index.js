e6933f016a570a37374897b3529073013f6da64a
nicolas-briemant <nicolas.briemant@gmail.com>
2014-01-29 18:35:40 +0100

    add phantom specific

---
+++ lib-phantom/index.js
@@ -0,0 +1,21 @@
+var system = require('system')
+  , page = require('webpage').create();
+
+var args = ['in', 'out'].reduce(function(args, name, i) {
+  args[name] = system.args[i+1];
+  return args
+}, {});
+
+page.open(args.in, function(status) {
+  if (status == "fail") {
+    page.close();
+    phantom.exit(1);
+    return;
+  }
+
+  setTimeout(function () {
+    page.render(args.out);
+    page.close();
+    phantom.exit(0);
+  }, 200);
+});
