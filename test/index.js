85b3fb9657b6eae20e0a92c739559309cae211dd
nicolas-briemant <nicolas.briemant@gmail.com>
2014-02-13 18:04:13 +0100

    add more complex test

--- test/index.js
+++ test/index.js
@@ -10,25 +10,33 @@ var jadepdf = require('../')
 
 tmp.setGracefulCleanup();
 
-describe('simple hello.jade without locals to pdf', function() {
-  it('should generate a non-empty PDF', function(done) {
-    this.timeout(5000);
+function helper(path, done) {
+  tmp.file({postfix: '.pdf'}, function(err, tmpPdfPath, tmpPdfFd) {
+    should.not.exist(err);
+    fs.close(tmpPdfFd);
 
-    tmp.file({postfix: '.pdf'}, function(err, tmpPdfPath, tmpPdfFd) {
-      should.not.exist(err);
-      fs.close(tmpPdfFd);
+    var outputStream = fs.createWriteStream(tmpPdfPath);
 
-      var outputStream = fs.createWriteStream(tmpPdfPath);
+    fs.createReadStream(__dirname+path).pipe(jadepdf()).pipe(outputStream);
 
-      fs.createReadStream(__dirname+'/hello.jade').pipe(jadepdf()).pipe(outputStream);
-
-      outputStream.on('finish', function() {
-        fs.readFile(tmpPdfPath, {encoding: 'utf8'}, function (err, data) { 
-          should.not.exist(err);
-          data.length.should.be.above(0);
-          done();
-        });
+    outputStream.on('finish', function() {
+      fs.readFile(tmpPdfPath, {encoding: 'utf8'}, function (err, data) { 
+        should.not.exist(err);
+        data.length.should.be.above(0);
+        done();
       });
     });
+  });
+}
+
+describe('simple jade file without locals to pdf', function() {
+  it('should generate a non-empty PDF', function(done) {
+    helper('/simple/index.jade', done);
+  });
+});
+
+describe('complex jade file without locals to pdf', function() {
+  it('should generate a non-empty PDF', function(done) {
+    helper('/complex/index.jade', done);
   });
 });
